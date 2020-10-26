8ed7d9c1d7a3ffc327257cb53ef7d8215b074733
Iddings Caleb <824881@stma.k12.mn.us>
2020-10-26 17:58:24 -0500

    Added basic operations practice

--- Java Practice/src/app/App.java
+++ Java Practice/src/app/App.java
@@ -2,6 +2,26 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        System.out.println("Star Wars: A New Hope");
+        System.out.println("Star Wars: Revenge of the Sith");
+        System.out.println("Star Wars: Return of the Jedi");
+        System.out.println("The Bee Movie");
+        System.out.println("Shrek");
+        int myFirstNumber = 1;
+        int mySecondNumber = 2;
+        System.out.println(myFirstNumber + mySecondNumber);
+        if ((mySecondNumber + myFirstNumber) >= 10) {
+            System.out.println("Greater than or equal to ten");
+        } else {
+            System.out.println("Less than ten");
+        }
+        int potato = 0;
+        while (potato < 1000) {
+            potato++;
+            System.out.println("Potato equals " + potato);
+        }
+        for (int potatoTwo = 0; potatoTwo < 1000; potatoTwo++) {
+            System.out.println("Potato 2 = " + potatoTwo);
+        }
     }
 }
